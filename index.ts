1fdcae573438d83f1c0d98c22f47e6b175788c3f
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-30 20:28:10 +0900

    chore: socket導入、tsコンパイル設定

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Socket } from "socket.io";
 
 dotenv.config();
 
 const app: Express = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
 
-
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
